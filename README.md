20cfeb8017a08bef85a11f0cc15bed6d103d8267
Bulat Farrakhov <bulat.farrakhov@skuvault.com>
2020-03-06 08:24:19 +0300

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# NewEgg
+NewEgg Rest API .NET wrapper (https://developer.newegg.com/newegg_marketplace_api/)
---
+++ src/NewEggTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NewEggTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SkuVault Inc.")]
+[assembly: AssemblyProduct("NewEggTests")]
+[assembly: AssemblyCopyright("Copyright © SkuVault Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cffd9882-5c11-45c0-a7d3-7ce31d154daa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
